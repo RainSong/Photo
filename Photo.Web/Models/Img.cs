afa44e2d15b1105c4df4400c6d7789ec286bf7f5
RainSong <714224605@qq.com>
2015-08-07 14:57:37 +0800

    添加相关Model
    Signed-off-by: RainSong <714224605@qq.com>

---
+++ Photo.Web/Models/Img.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Photo.Web.Models
+{
+    public class Img
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+
+        public int PageId { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Path { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Url { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Height { get; set; }
+        /// <summary>
+        /// 显示的宽度
+        /// </summary>
+        public int DisplayWidth
+        {
+            get { return 635; }
+        }
+        /// <summary>
+        /// 显示的高度
+        /// </summary>
+        public int DisplayHeight
+        {
+            get
+            {
+                if (Width * Height == 0) return 0;
+                return (Height * DisplayWidth) / Width;
+            }
+        }
+    }
+}
---
+++ Photo.Web/Models/Page.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Photo.Web.Models
+{
+    public class Page
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public IEnumerable<Tag> Tags { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public IEnumerable<Img> Imgs { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Img DefaultImg { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime AddTime { get; set; }
+    }
+}
